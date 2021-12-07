27234bb7cc94d069e88b750c97b4925c6adc65eb
Valentine <ifeanyivalentine82@gmail.com>
2021-12-07 15:15:18 +0100

    page setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import FirstPage from './components/FirstPage/FirstPage';
+
+
 
 function App() {
   return (
     <div className="App">
-      
+       <FirstPage/>
     </div>
   );
 }
---
+++ src/components/FirstPage/FirstPage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function FirstPage() {
+    return (
+        <div className='main'>
+            <div className='section'>
+                <div className='left'>
+                    
+                </div>
+                <div className='right'>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FirstPage
