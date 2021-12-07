0c4d8c611afa370ed0869f91ecbf902f289f8533
Valentine <ifeanyivalentine82@gmail.com>
2021-12-07 16:09:42 +0100

    latest commit

---
+++ src/components/Content.js
@@ -0,0 +1,15 @@
+const title = "Shunem Home Ideas";
+
+const landingimage = "assets/shunnen.jpeg"
+
+const landingtext = {
+    firsttext: "INTERIORS THAT REFLECT YOUR STYLE",
+    secondtext: "At Shunem Home Ideas, we give you the best designs with professionalism and at  pocket-frienly prices too"
+}
+
+
+export {
+    title,
+    landingimage,
+    landingtext,
+}
--- src/components/FirstPage/FirstPage.js
+++ src/components/FirstPage/FirstPage.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React from 'react';
+import '../FirstPage/FirstPage.scss'
+
+import {
+    landingimage, 
+    landingtext 
+} from '../Content'
 
 function FirstPage() {
     return (
         <div className='main'>
             <div className='section'>
                 <div className='left'>
-                    
+                    <img src={landingimage} alt='landing img'/>
                 </div>
                 <div className='right'>
-
+                    <div className='firsttext'>
+                        <h2>{landingtext.firsttext}</h2>
+                        <p5>{landingtext.secondtext}</p5>
+                    </div>
                 </div>
             </div>
         </div>
